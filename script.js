a69bb4352c7787d7c135f88bc4e2cef0d3e5cda9
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-20 18:17:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+let btn=document.getElementById('btn');
+
+function myPromise(value,time) {
+	return new Promise((res,rej)=>{
+		setTimeout(()=>{
+			res(value);
+		},time);
+	})
+}
+
+btn.addEventListener('click',async ()=>{
+	let v=document.querySelector('#text');
+	let d=document.querySelector('#delay');
+	let text=v.value;
+	let delay=d.value;
+	v.value='';
+	d.value='';
+	let result=await myPromise(text,delay);
+	let output=document.querySelector('#output')
+	output.textContent=result;
+	
+})
